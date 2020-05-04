c015eeeaceec27d00c0f73f3a3bd890bbe1b6ed8
nevtay <nevintay@gmail.com>
2020-05-04 23:37:43 +0800

    Update learning progress

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React, { useState } from "react";
-import { useForm } from "./useForm";
+import React, {useState, useEffect} from 'react';
+import {useForm} from './useForm';
+import {Hello} from './Hello';
 
 const App = () => {
-  const [values, handleChange] = useForm({ email: "", password: "" });
-  const [values2, handleChange2] = useForm({ firstName: "", lastName: "" });
+  const [values, handleChange] = useForm({
+    email: '',
+    password: '',
+    firstName: '',
+  });
+  // const [values2, handleChange2] = useForm({
+  //   firstName: '',
+  //   lastName: '',
+  // });
+  // const [showHello, setShowHello] = useState(true);
+
+  useEffect(() => {
+    console.log('render');
+
+    return () => {
+      console.log('unmount');
+    };
+  }, [values.email]);
 
   return (
     <div>
       <>
+        {/* <button
+          onClick={() => setShowHello(!showHello)}>
+        Button Here
+        </button>
+        {showHello ? <Hello /> : ''} */}
         <input name="email" value={values.email} onChange={handleChange} />
+        <input
+          name="firstName"
+          placeholder="first name"
+          value={values.firstName}
+          onChange={handleChange} />
         <input
           type="password"
           name="password"
+          placeholder="password"
           value={values.password}
           onChange={handleChange}
         />
