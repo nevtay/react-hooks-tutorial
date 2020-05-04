91e8666359b40c365fe6081f04f2b984666e23e1
nevtay <nevintay@gmail.com>
2020-05-05 00:02:41 +0800

    Add custom hook and run it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {useForm} from './useForm';
-import {Hello} from './Hello';
+// import {Hello} from './Hello';
+import {useFetch} from './useFetch';
 
 const App = () => {
   const [values, handleChange] = useForm({
@@ -8,19 +9,36 @@ const App = () => {
     password: '',
     firstName: '',
   });
+  const {data, loading} = useFetch('http://numbersapi.com/43/trivia');
   // const [values2, handleChange2] = useForm({
   //   firstName: '',
   //   lastName: '',
   // });
   // const [showHello, setShowHello] = useState(true);
 
-  useEffect(() => {
-    console.log('render');
+  // useEffect(() => {
+  //   const onMouseMove = (e) => {
+  //     console.log(e);
+  //   };
+  //   // addEventListener must have 2 arguments
+  //   window.addEventListener('mousemove', onMouseMove);
 
-    return () => {
-      console.log('unmount');
-    };
-  }, [values.email]);
+  //   return () => {
+  //     console.log('unmount');
+  //     window.removeEventListener('mousemove');
+  //   };
+  // }, []);
+
+
+  // http://numbersapi.com/43/trivia
+
+  useEffect(() => {
+    console.log('mount 1');
+  });
+
+  useEffect(() => {
+    console.log('mount 2');
+  });
 
   return (
     <div>
@@ -30,6 +48,7 @@ const App = () => {
         Button Here
         </button>
         {showHello ? <Hello /> : ''} */}
+        <div>{loading ? 'LOADING...' : data}</div>
         <input name="email" value={values.email} onChange={handleChange} />
         <input
           name="firstName"
