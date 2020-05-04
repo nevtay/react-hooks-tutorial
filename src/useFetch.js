9ff571132844d3c8273f7bdc5d63a890328351b3
nevtay <nevintay@gmail.com>
2020-05-05 00:02:19 +0800

    Initial commit

---
+++ src/useFetch.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+import React, {useState, useEffect} from 'react';
+
+export const useFetch = (url) => {
+  const [state, setState]= useState({
+    data: null,
+    loading: true,
+  });
+
+  useEffect(() => {
+    setState({data: null, loading: true});
+    fetch(url)
+        .then((x) => x.text())
+        .then((y) => {
+          setState({data: y, loading: false});
+        });
+  }, [url]);
+  return state;
+};
